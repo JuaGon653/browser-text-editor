44320a5ca58fdc34e091b12278a6403dbf221df2
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-02 12:42:52 -0500

    created PUT and GET request for indexDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  // connection to the database with the version we want to use
+  const jateDb = await openDB('jate', 1);
+  // new transaction and indicate the database and data privileges
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // open the indicated object store
+  const store = tx.objectStore('jate');
+  // put data in the indexDB
+  const request = store.put({ id: id, jate: content });
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET all from the database');
+  
+  const jateDb = await openDB('jate', 1);
+  
+  const tx = jateDb.transaction('jate', 'readonly');
+  
+  const store = tx.objectStore('jate');
+  // get all data in database
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
