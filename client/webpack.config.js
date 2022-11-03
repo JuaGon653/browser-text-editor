674cb27eb734b8b769013dc0e733dda6eb72af2d
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-11-03 16:23:13 -0500

    multiple sizes of the logo

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -38,7 +38,7 @@ module.exports = () => {
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: '96x96',
+            sizes: [96, 128, 192, 256, 512],
             destination: path.join('assets', 'icons')
           }
         ]
